c175fe9cbc41886165c3a149b4609d4978cbc625
SmudgedWings <2045955563@qq.com>
2024-07-30 13:19:04 +0800

    aaa

---
+++ ci_check/awq.py
@@ -0,0 +1,221 @@
+import torch
+import torch.nn as nn
+from loguru import logger
+import gc
+from .module_utils import _LLMC_LN_TYPES_, _TRANSFORMERS_LN_TYPES_
+from .module_utils import _LLMC_LINEAR_TYPES_, _TRANSFORMERS_LINEAR_TYPES_
+from .module_utils import FakeQuantLinear
+from .base_blockwise_quantization import BaseBlockwiseQuantization
+from .utils import get_wquantizer, get_aquantizer, check_do_quant, check_w_only
+from llmc.utils.registry_factory import ALGO_REGISTRY
+
+n_grid_zbl = 1
+
+@ALGO_REGISTRY
+class Awq(BaseBlockwiseQuantization):
+    def __init__(self, model, quant_config, input, config):
+        super().__init__(model, quant_config, input, config)
+        special_config = self.quant_config.get("special", {})
+        self.trans = special_config.get("trans", True)
+        self.trans_version = special_config.get("trans_version", "v2")
+        self.weight_clip = special_config.get("weight_clip", True)
+        self.save_scale = special_config.get("save_scale", False)
+
+    @torch.no_grad()
+    def get_weight_scale(self, layers_dict):
+        layers = list(layers_dict.values())
+        weights = self.collect_layers_weights(layers)
+        weights = torch.cat(weights, dim=0)
+        org_shape = weights.shape
+        wquantizer = get_wquantizer(
+            self.block_idx,
+            list(layers_dict.keys())[0],
+            self.mix_bits_map,
+            self.quantizer_mix_bits,
+            self.wquantizer,
+        )
+        weights = wquantizer.reshape_tensor(weights)
+        scale = weights.abs() / weights.abs().amax(dim=1, keepdim=True)
+        scale = scale.view(org_shape)
+        scale = scale.mean(0)
+        del weights
+        gc.collect()
+        torch.cuda.empty_cache()
+        return scale
+
+    @torch.no_grad()
+    def get_act_scale(self, x):
+        return x.abs().view(-1, x.shape[-1]).mean(0)
+
+    @torch.no_grad()
+    def get_original_out(self, x, inspect_module, subset_kwargs):
+        with torch.no_grad():
+            org_out = inspect_module(x, **subset_kwargs)
+            if isinstance(org_out, tuple):
+                org_out = org_out[0]
+        return org_out
+
+    @torch.no_grad()
+    def search_scale_subset(self, layers_dict, input, inspect_module, subset_kwargs):
+        w_max = self.get_weight_scale(layers_dict)
+        # grid search for ratio
+        best_error = float("inf")
+        best_scales = None
+        n_grid = n_grid_zbl  # 20
+        org_sd = {k: v.cpu() for k, v in inspect_module.state_dict().items()}
+
+        org_out_dict = {}
+        for n in range(n_grid):
+            loss_mean = 0
+            scales_mean = 0
+            for i in range(len(input)):
+                input[i] = input[i].to(next(inspect_module.parameters()).device)
+                x = input[i]
+                if isinstance(subset_kwargs, list):
+                    kwargs = subset_kwargs[i]
+                else:
+                    kwargs = subset_kwargs
+                if i in org_out_dict:
+                    org_out = org_out_dict[i]
+                else:
+                    org_out = self.get_original_out(x, inspect_module, kwargs)
+                    org_out_dict[i] = org_out
+                x_max = self.get_act_scale(x)
+
+                ratio = n * 1 / n_grid
+                if self.trans_version == "v1":
+                    scales = (
+                        (x_max.pow(ratio) / w_max.pow(1 - ratio))
+                        .clamp(min=1e-4)
+                        .view(-1)
+                    )
+                elif self.trans_version == "v2":
+                    scales = x_max.pow(ratio).clamp(min=1e-4).view(-1)
+                scales = scales / (scales.max() * scales.min()).sqrt()
+                for layer_name in layers_dict:
+                    fc = layers_dict[layer_name]
+                    fc.weight.mul_(scales.view(1, -1))
+
+                    fc.weight.data = get_wquantizer(
+                        self.block_idx,
+                        layer_name,
+                        self.mix_bits_map,
+                        self.quantizer_mix_bits,
+                        self.wquantizer,
+                    ).fake_quant_weight_dynamic(fc.weight.data)
+
+                x_tmp = x / scales.view(1, -1)
+                if not check_w_only(
+                    self.block_idx,
+                    list(layers_dict.keys())[0],
+                    self.mix_bits_map,
+                    self.quantizer_mix_bits,
+                    self.w_only,
+                ):
+                    x_tmp = get_aquantizer(
+                        self.block_idx,
+                        list(layers_dict.keys())[0],
+                        self.mix_bits_map,
+                        self.quantizer_mix_bits,
+                        self.aquantizer,
+                    ).fake_quant_act_dynamic(x_tmp)
+
+                out = inspect_module(x_tmp, **kwargs)
+
+                if isinstance(out, tuple):
+                    out = out[0]
+
+                loss = (org_out - out).float().pow(2).mean().item()
+                loss_mean += x.shape[0] * 1.0 / self.n_samples * loss
+                scales_mean += x.shape[0] * 1.0 / self.n_samples * scales
+                inspect_module.load_state_dict(org_sd)
+            is_best = loss_mean < best_error
+            if is_best:
+                best_error = loss_mean
+                best_scales = scales_mean
+        best_scales = best_scales.view(-1)
+        del org_out_dict
+        gc.collect()
+        torch.cuda.empty_cache()
+        return best_scales
+
+    @torch.no_grad()
+    def update_input_feat(self, scale, input_feat, layers_dict):
+        for layer_name in layers_dict:
+            for i in range(len(input_feat[layer_name])):
+                inp = input_feat[layer_name][i]
+                inp.div_(scale.view(1, -1).to(inp.device))
+
+    @torch.no_grad()
+    def block_transform(self, block, input_feat, block_kwargs):
+        if self.trans:
+            super().block_transform(block, input_feat, block_kwargs)
+
+        if self.weight_clip:
+            logger.info(f"auto_clip start")
+            logger.info(f"clip version: {self.clip_version}")
+            self.auto_clip(block, input_feat, n_sample_token=self.config.calib.seq_len)
+            logger.info(f"auto_clip finished")
+        else:
+            logger.info(f"disable weight clip")
+
+    @torch.no_grad()
+    def subset_transform(
+        self,
+        layers_dict,
+        input_feat,
+        prev_op,
+        input_name,
+        inspect_module,
+        subset_kwargs,
+    ):
+        if not check_do_quant(
+            self.block_idx,
+            list(layers_dict.keys())[0],
+            self.mix_bits_map,
+            self.quantizer_mix_bits,
+        ): 
+            logger.info(
+                "This subset is set to float. No need to transform this subset."
+            )
+            return
+        if self.config["model"]["type"] == "Starcoder":
+            if isinstance(prev_op[0], (nn.Linear, FakeQuantLinear)):
+                logger.info("Do not transform this subset.")
+                return
+
+        assert (
+            len(prev_op) == 1
+        ), "Only support single prev_op. If multi prev_ops, code need to be updated."
+
+        if isinstance(
+            prev_op[0],
+            tuple(
+                _LLMC_LN_TYPES_
+                + _TRANSFORMERS_LN_TYPES_
+                + _LLMC_LINEAR_TYPES_
+                + _TRANSFORMERS_LINEAR_TYPES_
+            ),
+        ):
+            layers = list(layers_dict.values())
+
+            if (
+                isinstance(prev_op[0], (nn.Linear, FakeQuantLinear))
+                and prev_op[0].out_features != layers[0].in_features * 3
+                and prev_op[0].out_features != layers[0].in_features
+            ):
+                logger.info("Cannot apply scale. Do not transform this subset.")
+                return
+            
+            scale = self.search_scale_subset(
+                layers_dict, input_feat[input_name], inspect_module, subset_kwargs
+            )
+            self.apply_scale(scale, prev_op, layers)
+            self.update_input_feat(scale, input_feat, layers_dict)
+
+            if self.save_scale:
+                for n in layers_dict:
+                    layer_name = f"{self.model.block_name_prefix}.{self.block_idx}.{n}"
+                    self.act_scales[layer_name] = scale
+        else:
+            logger.info("Do not transform this subset.")
---
+++ ci_check/base_model.py
@@ -0,0 +1,267 @@
+from abc import abstractmethod, ABCMeta
+import gc
+import torch
+import torch.nn as nn
+from transformers import AutoModelForCausalLM, AutoConfig
+from loguru import logger
+from functools import partial
+from collections import defaultdict
+from llmc.compression.quantization.utils import check_do_quant
+from llmc.compression.quantization.module_utils import (
+    _LLMC_LN_TYPES_,
+    _TRANSFORMERS_LN_TYPES_,
+)
+from llmc.compression.quantization.module_utils import (
+    _LLMC_LINEAR_TYPES_,
+    _TRANSFORMERS_LINEAR_TYPES_,
+)
+from llmc.compression.quantization.utils import (
+    get_wquantizer,
+    get_aquantizer,
+    check_w_only,
+)
+
+device_zbl = "cpu"  # cuda & cpu
+use_cuda = (device_zbl!="cpu")
+
+class BaseModel(metaclass=ABCMeta):
+    def __init__(self, model_path, torch_dtype):
+        self.model_path = model_path
+        self.torch_dtype = torch_dtype if torch_dtype == "auto" else eval(torch_dtype)
+        self.build_model()
+        self.model.eval()
+        self.find_blocks()
+        self.find_embed_layers()
+        self.find_block_name()
+
+    @abstractmethod
+    def find_blocks(self):
+        pass
+
+    def find_block_name(self):
+        pass
+
+    def get_model(self):
+        return self.model
+
+    def get_blocks(self):
+        return self.blocks
+
+    @abstractmethod
+    def find_embed_layers(self):
+        pass
+
+    @abstractmethod
+    def get_embed_layers(self):
+        pass
+
+    @abstractmethod
+    def get_layers_except_blocks(self):
+        pass
+
+    @abstractmethod
+    def get_subsets_in_block(self, block):
+        pass
+
+    @abstractmethod
+    def has_bias(self):
+        pass
+
+    def __str__(self):
+        return f"\nConfig: \n{str(self.model_config)} \nModel: \n{str(self.model)}"
+
+    def build_model(self):
+        self.model_config = AutoConfig.from_pretrained(
+            self.model_path, trust_remote_code=True
+        )
+        if hasattr(self.model_config, "use_cache"):
+            self.model_config.use_cache = False
+        logger.info(f"self.model_config : {self.model_config}")
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.model_path,
+            config=self.model_config,
+            trust_remote_code=True,
+            torch_dtype=self.torch_dtype,
+            low_cpu_mem_usage=True,
+        )
+
+    @torch.no_grad()
+    def collect_first_block_input(self, calib_data):
+        first_block_input = defaultdict(list)
+
+        class Catcher(nn.Module):
+            def __init__(self, module):
+                super().__init__()
+                self.module = module
+
+            def forward(self, inp, **kwargs):
+                first_block_input["data"].append(inp)
+                if "output_router_logits" in kwargs:
+                    assert kwargs["output_router_logits"] == False
+                    kwargs.pop("output_router_logits")
+                first_block_input["kwargs"].append(kwargs)
+                raise ValueError
+
+        # self.move_embed_to_device("cuda")
+        # self.blocks[0] = self.blocks[0].cuda()
+        self.move_embed_to_device(device_zbl)
+        self.blocks[0] = self.blocks[0].to(device_zbl)
+        self.blocks[0] = Catcher(self.blocks[0])
+
+        for data in calib_data:
+            try:
+                self.model(data.to(next(self.model.parameters()).device))
+            except ValueError:
+                pass
+        self.first_block_input = first_block_input
+        self.blocks[0] = self.blocks[0].module
+        self.blocks[0] = self.blocks[0].cpu()
+        self.move_embed_to_device("cpu")
+
+    def get_first_block_input(self):
+        return self.first_block_input
+
+    def get_model_config(self):
+        return self.model_config
+
+    def move_embed_to_device(self, device):
+        for embed_layer in self.get_embed_layers():
+            embed_layer = embed_layer.to(device)
+
+    def get_block_linears(self, block):
+        return {
+            name: m
+            for name, m in block.named_modules()
+            if isinstance(m, tuple(_LLMC_LINEAR_TYPES_ + _TRANSFORMERS_LINEAR_TYPES_))
+        }
+
+    def set_mix_bits_params_dict(self, block_idx, name, params_dict):
+
+        logger.info("set_mix_bits_params_dict")
+
+        if not check_do_quant(
+            block_idx,
+            name,
+            params_dict["mix_bits_map"],
+            params_dict["quantizer_mix_bits"],
+        ):
+            logger.info(
+                f"This layer {name} in {block_idx}-th block is set to float. No need to replace this layer."
+            )
+            return params_dict
+
+        params_mix_dict = {}
+        params_mix_dict["debug_print"] = {}
+        wquantizer = get_wquantizer(
+            block_idx,
+            name,
+            params_dict["mix_bits_map"],
+            params_dict["quantizer_mix_bits"],
+            params_dict["wquantizer_default"],
+        )
+        params_mix_dict["w_qdq"] = partial(params_dict["w_qdq"], wquantizer=wquantizer)
+        params_mix_dict["debug_print"]["weight"] = {}
+        params_mix_dict["debug_print"]["weight"]["bit"] = wquantizer.bit
+        params_mix_dict["debug_print"]["weight"]["sym"] = wquantizer.sym
+        params_mix_dict["debug_print"]["weight"]["granularity"] = wquantizer.granularity
+        if wquantizer.granularity == "per_group":
+            params_mix_dict["debug_print"]["weight"][
+                "group_size"
+            ] = wquantizer.group_size
+        if not check_w_only(
+            block_idx,
+            name,
+            params_dict["mix_bits_map"],
+            params_dict["quantizer_mix_bits"],
+            params_dict["w_only_default"],
+        ):
+            aquantizer = get_aquantizer(
+                block_idx,
+                name,
+                params_dict["mix_bits_map"],
+                params_dict["quantizer_mix_bits"],
+                params_dict["aquantizer_default"],
+            )
+            params_mix_dict["a_qdq"] = partial(
+                params_dict["a_qdq"], aquantizer=aquantizer
+            )
+            params_mix_dict["debug_print"]["act"] = {}
+            params_mix_dict["debug_print"]["act"]["bit"] = aquantizer.bit
+            params_mix_dict["debug_print"]["act"]["sym"] = aquantizer.sym
+            params_mix_dict["debug_print"]["act"][
+                "granularity"
+            ] = aquantizer.granularity
+        else:
+            params_mix_dict["a_qdq"] = None
+        return params_mix_dict
+
+    def replace_module_all(self, module, params_dict):
+        for block_idx in range(len(self.blocks)):
+            logger.info(f"Replace block index: {block_idx}/{len(self.blocks)}")
+            block = self.blocks[block_idx]
+            block = block.to(device_zbl)
+            self.replace_module_block(module, block, block_idx, params_dict)
+            block = block.cpu()
+
+        gc.collect()
+        torch.cuda.empty_cache()
+        logger.info(f"The Replaced model: {self.model}")
+
+    def replace_module_block(self, module, block, block_idx, params_dict):
+        if module in _LLMC_LN_TYPES_ + _TRANSFORMERS_LN_TYPES_:
+            layer_norms = self.get_layernorms_in_block(block)
+            self.replace_module_layernorm(
+                module, block, layer_norms, block_idx, params_dict
+            )
+        else:
+            subset = {}
+            subset["layers"] = self.get_block_linears(block)
+            self.replace_module_subset(module, block, subset, block_idx, params_dict)
+
+    def replace_module_subset(self, module, block, subset, block_idx, params_dict):
+        layers_dict = subset["layers"]
+
+        for name, m in layers_dict.items():
+            # mix bits
+            params_tmp_dict = {}
+            if "mix_bits" in params_dict and params_dict["mix_bits"]:
+                params_tmp_dict = self.set_mix_bits_params_dict(
+                    block_idx, name, params_dict
+                )
+            else:
+                params_tmp_dict = params_dict
+
+            M = module.new(m, **params_tmp_dict)
+
+            name_tmp = name.rsplit(".", 1)
+            if len(name_tmp) == 2:
+                parent_name = name_tmp[0]
+                parent = block.get_submodule(parent_name)
+                child_name = name_tmp[1]
+            elif len(name_tmp) == 1:
+                parent = block
+                child_name = name_tmp[0]
+
+            setattr(parent, child_name, M)
+            logger.info(f"replace >>> {name} in {block_idx}-th block")
+
+    def replace_module_layernorm(self, module, block, lns, i, params_dict):
+        for name, m in lns.items():
+            if isinstance(m, module):
+                continue
+            M = module.new(m, **params_dict)
+
+            name_tmp = name.rsplit(".", 1)
+            if len(name_tmp) == 2:
+                parent_name = name_tmp[0]
+                parent = block.get_submodule(parent_name)
+                child_name = name_tmp[1]
+            elif len(name_tmp) == 1:
+                parent = block
+                child_name = name_tmp[0]
+
+            setattr(parent, child_name, M)
+
+    def convert_dtype(self, dtype="torch.float16"):
+        for i in range(len(self.blocks)):
+            self.blocks[i] = self.blocks[i].to(dtype)
