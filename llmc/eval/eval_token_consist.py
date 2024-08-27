14c7ed1eb5b7f581c434d56be0c0bb49100a3417
gushiqiao <gushiqiao@sensetime.com>
2024-08-27 13:41:36 +0800

    Fix eval bug

--- llmc/eval/eval_token_consist.py
+++ llmc/eval/eval_token_consist.py
@@ -10,26 +10,26 @@ from .eval_base import BaseEval
 class TokenConsistencyEval(BaseEval):
 
     @torch.no_grad()
-    def eval_func(self, model1, model2):
-        self.testenc = self.testenc.input_ids
-        nsamples = self.testenc.numel() // self.seq_len
+    def eval_func(self, org_model, model, testenc, seq_len, bs):
+        testenc = testenc.input_ids
+        nsamples = testenc.numel() // seq_len
 
         consistent_tokens = 0
         total_tokens = 0
 
         # Loop through each batch
-        for i in range(0, nsamples, self.bs):
-            logger.info(f'index : {(i + 1) // self.bs}/{nsamples // self.bs}')
+        for i in range(0, nsamples, bs):
+            logger.info(f'index : {(i + 1) // bs}/{nsamples // bs}')
             # Calculate end index
-            j = min(i + self.bs, nsamples)
+            j = min(i + bs, nsamples)
 
             # Prepare inputs and move to gpu
-            inputs = self.testenc[:, (i * self.seq_len): (j * self.seq_len)].cuda()
-            inputs = inputs.reshape(j - i, self.seq_len)
+            inputs = testenc[:, (i * seq_len): (j * seq_len)].cuda()
+            inputs = inputs.reshape(j - i, seq_len)
 
             # Forward pass through the models
-            logits1 = model1(inputs).logits
-            logits2 = model2(inputs).logits
+            logits1 = org_model(inputs).logits
+            logits2 = model(inputs).logits
 
             # Get predicted tokens
             preds1 = torch.argmax(logits1, dim=-1)
@@ -42,7 +42,7 @@ class TokenConsistencyEval(BaseEval):
         consistency_ratio = consistent_tokens / total_tokens
 
         # Empty CUDA cache to save memory
-        self.testenc.cpu()
+        testenc.cpu()
         torch.cuda.empty_cache()
 
         return consistency_ratio
@@ -67,8 +67,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     tokenizer = BaseTokenizer(args.model_path_1, tokenizer_mode="slow")
-    model1 = MODEL_REGISTRY[args.model_type_1](args.model_path_1, 'auto')
-    model2 = MODEL_REGISTRY[args.model_type_2](args.model_path_2, 'auto')
+    org_model = MODEL_REGISTRY[args.model_type_1](args.model_path_1, 'auto')
+    model = MODEL_REGISTRY[args.model_type_2](args.model_path_2, 'auto')
 
     eval_cfg = {
         'name': 'wikitext2',
@@ -80,5 +80,5 @@ if __name__ == '__main__':
     }
     token_consistency_eval = TokenConsistencyEval(tokenizer.get_tokenizer(), eval_cfg)
 
-    consistency_ratio = token_consistency_eval.eval(model1, model2)
+    consistency_ratio = token_consistency_eval.eval(org_model, model)
     logger.info(f'Token consistency ratio: {consistency_ratio}')
