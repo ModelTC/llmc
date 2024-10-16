204759a710feb2ad00466e7c15636d4f860d4bc6
MercuryB1 <wangzining@buaa.edu.cn>
2024-10-16 15:10:27 +0800

    add qwen2moe

--- llmc/models/__init__.py
+++ llmc/models/__init__.py
@@ -13,6 +13,7 @@ from .opt import Opt
 from .phi import Phi
 from .qwen import Qwen
 from .qwen2 import Qwen2
+from .qwen2moe import Qwen2Moe
 from .qwenvl import QwenVL
 from .smollm import SmolLM
 from .stablelm import StableLm
