179a06738cece106e9f60554c96255fc0f866b4b
SmudgedWings <2045955563@qq.com>
2025-05-23 00:39:26 +0800

    support llava-lht（test：quantize-awq, MME-pretrain)

--- llmc/models/__init__.py
+++ llmc/models/__init__.py
@@ -10,6 +10,7 @@ from .internomni import InternOmni
 from .internvl2 import InternVL2
 from .llama import Llama
 from .llava import Llava
+from .llava_lht import LlavaLHT
 from .minicpm import MiniCPM
 from .minicpmv import MiniCPMV
 from .mistral import Mistral
